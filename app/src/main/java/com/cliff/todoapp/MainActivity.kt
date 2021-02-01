dafef8c535d7e46500b1e006c466338fe574f273
cliffgor <clierozigy@gmail.com>
2021-02-01 19:34:37 +0300

    Added the row itmes activity and also designed the lists layout on how they will look like

--- app/src/main/java/com/cliff/todoapp/MainActivity.kt
+++ app/src/main/java/com/cliff/todoapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.cliff.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fab=findViewById<View>(R.id.fab)
     }
 }
