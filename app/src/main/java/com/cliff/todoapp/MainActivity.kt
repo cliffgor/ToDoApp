809d8155f4bac2fb91c31c4b3208e49ca5abf2ad
cliffgor <clierozigy@gmail.com>
2021-02-01 19:49:06 +0300

    added dialogue pop up when you click the action bar button to add your todo item

--- app/src/main/java/com/cliff/todoapp/MainActivity.kt
+++ app/src/main/java/com/cliff/todoapp/MainActivity.kt
@@ -3,12 +3,25 @@ package com.cliff.todoapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fab=findViewById<View>(R.id.fab)
+        val fab=findViewById<View>(R.id.fab) as FloatingActionButton
+
+        fab.setOnClickListener { View ->
+            val alertDialog = AlertDialog.Builder(this)
+            val textEditText = EditText(this)
+            alertDialog.setMessage("Add your todo Item")
+            alertDialog.setTitle("Enter Todo Item")
+            alertDialog.setView(textEditText)
+            alertDialog.show()
+
+        }
     }
 }
